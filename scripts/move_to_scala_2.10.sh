92a07f2a8397d9e1332c7eef3804ad4c64b18b51
Michael L Heuer <heuermh@acm.org>
2016-10-18 15:56:42 -0500

    bump adam dependency version to Spark 2 release version 0.20.0

--- scripts/move_to_scala_2.10.sh
+++ scripts/move_to_scala_2.10.sh
@@ -5,7 +5,7 @@ set +x
 find . -name "pom.xml" -exec sed \
      -e "s/\<artifactId\>adam-examples_2.11\<\/artifactId\>/\<artifactId\>adam-examples_2.10\<\/artifactId\>/g" \
      -e "s/\<name\>adam-examples_2.11\<\/name\>/\<name\>adam-examples_2.10\<\/name\>/g" \
-     -e "s/\<scala.version\>2.11.4\<\/scala.version\>/\<scala.version\>2.10.4\<\/scala.version\>/g" \
+     -e "s/\<scala.version\>2.11.8\<\/scala.version\>/\<scala.version\>2.10.5\<\/scala.version\>/g" \
      -e "s/\<scala.artifact.suffix\>2.11\<\/scala.artifact.suffix\>/\<scala.artifact.suffix\>2.10\<\/scala.artifact.suffix\>/g" \
      -i .2.10.bak '{}' \;
 find . -name "*.2.10.bak" -exec rm {} \;
